2f7d91cec17e864a64c0e4f2e760b98d5ac8e04a
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-11 08:06:08 +0300

    Edit ex1.php to format and search for specific items in the log file

--- Week2/Tu_Assignments-v01BD_1603/ex1.php
+++ Week2/Tu_Assignments-v01BD_1603/ex1.php
@@ -1,17 +1,73 @@
+#!/usr/bin/php
 <?php
-//$options = getopt("a:e:t:d:s:m");
 
-exec("git log --format='%H - %an - %cd - \"%s\"'", $output);
+function search_for($options)
+{
+    $search = 'Search result by ';
+    foreach ($options as $key) {
+        switch ($key) {
+            case 'a':
+                $search .= "Author (\"$options[$key]\"), ";
+                break;
+            case 'e':
+                $search .= "Email address(\"$options[$key]\"), ";
+                break;
+            case 't':
+                $search .= "Time (\"$options[$key]\"), ";
+                break;
+            case 'd':
+                $search .= "Date (\"$options[$key]\"), ";
+                break;
+            case 's':
+                $search .= "Epoch timestamp (\"$options[$key]\"), ";
+                break;
+            case 'm':
+                $search .= "Commit message (\"$options[$key]\"), ";
+                break;
+        }
+    }
+    print("$search") . PHP_EOL;
+}
 
+function get_options()
+{
 
-//exec("git log",$output);
+    $options = getopt('a:e:t:d:s:m:');
+    $command = 'git log --format="%H - %an - %aD - %s"';
 
-$searchfor = "9-7-2018";
-foreach ($output as $value) {
-    if (strpos($value, $searchfor) !== false)
-    {
-        echo "$value".PHP_EOL;
+    if (!empty($options)) {
+        search_for($options);
+        if (array_key_exists('a', $options)) {
+            $command .= ' --author=' . $options['a'];
+        }
+        if (array_key_exists('e', $options)) {
+            $command .= ' --author=' . $options['e'];
+        }
+        if (array_key_exists('d', $options)) {
+            $date = new DateTime($options['d']);
+            $command .= ' --since=\'' . $date->format('m-d-Y H:i') . '\' --until=\''
+                . $date->format('m-d-Y 23:59') . '\'';
+        }
+        if (array_key_exists('s', $options)) {
+            $command .= ' --since=' . $options['s'] . ' --until=' . $options['s'];
+        }
+        if (array_key_exists('m', $options)) {
+            $command .= ' --grep=' . $options['m'];
+        }
+
+        $output = shell_exec($command);
+        $output = explode("\n", $output);
+        $n = 0;
+        foreach ($output as $line) {
+            if ($line != '') {
+                echo ++$n . ':: ' . $line . PHP_EOL;
+            }
+        }
+    } else {
+        echo "Options are incorrect" . PHP_EOL;
     }
 }
+
+get_options();
 
 ?>
