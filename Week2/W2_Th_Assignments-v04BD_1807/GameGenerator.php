9e86ab32e645c61e45c48f9f8109f97cda91c884
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-15 09:13:39 +0300

    Refactor GameGenerator and GameSolver classes

--- Week2/W2_Th_Assignments-v04BD_1807/GameGenerator.php
+++ Week2/W2_Th_Assignments-v04BD_1807/GameGenerator.php
@@ -8,32 +8,33 @@
 class GameGenerator
 {
 
-    public $FOUR_NUMBERS_ARRAY = array(25, 50, 75, 100);
-    public $TWENTY_NUMBERS_ARRAY = array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
+    public static $FOUR_NUMBERS_ARRAY = array(25, 50, 75, 100);
+    public static $TWENTY_NUMBERS_ARRAY = array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
         6, 6, 7, 7, 8, 8, 9, 9, 10, 10);
-    public $numbers_array = array();
+
     /**
      * Randomly pick 6 numbers from the two arrays FOUR_NUMBERS_ARRAY, and
      * TWENTY_NUMBERS_ARRAY
      * @return array
      */
-    function generating_numbers()
+    public static function generate_array_number()
     {
+//        TODO work on the styling
 
-        $random_number_four = rand(1, 4);
         $numbers_array = array();
+        $random_number_four = rand(1, 4);
 
 
 //        picking n numbers from the array of 4 elements
         for ($i = 0; $i <= $random_number_four; $i++) {
-            
+
             $random_number = rand(0, 3);
 
-            while (in_array($this->FOUR_NUMBERS_ARRAY[$random_number], $numbers_array) && count($numbers_array) <= 3) {
+            while (in_array(GameGenerator::$FOUR_NUMBERS_ARRAY[$random_number], $numbers_array) && count($numbers_array) <= 3) {
                 $random_number = rand(0, 3);
             }
             if (count($numbers_array) <= 3) {
-                array_push($numbers_array, $this->FOUR_NUMBERS_ARRAY[$random_number]);
+                array_push($numbers_array, GameGenerator::$FOUR_NUMBERS_ARRAY[$random_number]);
             }
 
         }
@@ -43,32 +44,28 @@ class GameGenerator
 
         $visited_index = array();
         for ($i = 0; $i <= $remaining_number - 1; $i++) {
-
             $random_number = rand(0, 19);
             if (in_array($random_number, $visited_index) == false) {
                 array_push($visited_index, $random_number);
-                array_push($numbers_array, $this->TWENTY_NUMBERS_ARRAY[$random_number]);
+                array_push($numbers_array, GameGenerator::$TWENTY_NUMBERS_ARRAY[$random_number]);
             } else {
 
                 while (in_array($random_number, $visited_index)) {
                     $random_number = rand(0, 19);
                 }
                 array_push($visited_index, $random_number);
-                array_push($numbers_array, $this->TWENTY_NUMBERS_ARRAY[$random_number]);
+                array_push($numbers_array, GameGenerator::$TWENTY_NUMBERS_ARRAY[$random_number]);
             }
-
-
         }
         return $numbers_array;
     }
-
 
 
     /**
      * Generate a random number of 3 digits
      * @return int
      */
-    function Random_3_digit()
+    public function Random_3_digit()
     {
         return rand(101, 999);
     }
--- Week2/W2_Th_Assignments-v04BD_1807/GameSolver.php
+++ Week2/W2_Th_Assignments-v04BD_1807/GameSolver.php
@@ -14,7 +14,7 @@ class GameSolver
                 $new_permutations = $perms;
                 list($foo) = array_splice($new_items, $i, 1);
                 array_unshift($new_permutations, $foo);
-                arrayPermutation($new_items, $new_permutations, $ret);
+                $this->arrayPermutation($new_items, $new_permutations, $ret);
             }
         }
         return $ret;
@@ -30,7 +30,7 @@ class GameSolver
         }
 
         // get combinations from subsequent arrays
-        $tmp = combinations($arrays, $i + 1);
+        $tmp = $this->arrayCombinations($arrays, $i + 1);
 
         $result = array();
 
@@ -46,27 +46,42 @@ class GameSolver
         return $result;
     }
 
-    public function getArrayNumbers()
-    {
-        $numbers = new GameGenerator();
-        $numbers->generating_numbers();
-        print_r($numbers);
-    }
+//    public function getArrayNumbers()
+//    {
+//        $numbers = new GameGenerator();
+//        $numbers->generating_numbers();
+//        print_r($numbers);
+//    }
 
+
+//TODO fix the call of functions of other class (generate_array_number)
+//The problem is that I'm not able to access the generate_array_number() from class GameGenerator
     public function joinArithmeticOperations()
     {
-        $permutations_array = arrayPermutation(generating_numbers());
+        $permutations_array = $this->arrayPermutation(GameGenerator::generate_array_number());
         print_r($permutations_array);
         $array_of_combinations = array();
         for ($i = 0; $i < sizeof($permutations_array); $i++) {
-            $array_of_combinations = combinations(array($permutations_array[$i], ["+", "-", "/", "*"], $permutations_array[$i]));
+            $array_of_combinations = $this->arrayCombinations(array($permutations_array[$i], ["+", "-", "/", "*"], $permutations_array[$i]));
         }
         for ($i = 0; $i < sizeof($array_of_combinations); $i++) {
             for ($j = 0; $j < 3; $j++) {
                 print_r($array_of_combinations[$i][$j]);
-
             }
             print("\n");
         }
     }
 }
+
+$game_generator = new GameGenerator();
+print("The generated Array number is ");
+print_r($generated_array = ($game_generator->generate_array_number()));
+print("The Expected number is ".$game_generator->Random_3_digit()).PHP_EOL;
+
+
+
+//$game_solver = new GameSolver();
+//$game_solver->joinArithmeticOperations();
+
+
+
