4f512521b0b81c454617902b97e8913446a91120
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-17 16:41:41 +0300

    Create and edit selectFromTable method in Database class

--- Week3/W3_Tu_Assignments-v03BD_1701/Database.php
+++ Week3/W3_Tu_Assignments-v03BD_1701/Database.php
@@ -2,13 +2,30 @@
 
 class Database
 {
+
     public $DATABASE_LOCATION = "./";
-    // default value
     public $latest_database_used = "NewDatabase";
+    public $latest_table_used = "student";
 
     function __construct()
     {
 //        TODO do something
+    }
+
+    /**
+     * @return string
+     */
+    public function getDATABASELOCATION()
+    {
+        return $this->DATABASE_LOCATION;
+    }
+
+    /**
+     * @param string $DATABASE_LOCATION
+     */
+    public function setDATABASELOCATION($DATABASE_LOCATION)
+    {
+        $this->DATABASE_LOCATION = $DATABASE_LOCATION;
     }
 
     /**
@@ -24,20 +41,24 @@ class Database
         } else {
             mkdir($this->DATABASE_LOCATION . $database_name, 0777);
             print("Database $database_name is created") . PHP_EOL;
-//            Assigning the lastest used database to that whenever we make a new
+//            Assigning the latest used database to that whenever we make a new
 //            operation, it is done on this database
-            $this->latest_database_used = $database_name;
+            $this->setLatestDatabaseUsed($database_name);
         }
     }
 
     /**
-     * delete the database by deleting all it files and folder recursively and
-     * then deleting the database
-     * @param $database_name
+     * delete database, if it receives params, it will take it, else, use
+     * latestDatabaseUsed
+     * @param string $database_name
      * @return bool
      */
-    function deleteDatabase($database_name = "NewDatabase")
+    function deleteDatabase($database_name = "")
     {
+        if ($database_name == "") {
+            $database_name = $this->getLatestDatabaseUsed();
+        }
+        $database_name = $this->getLatestDatabaseUsed();
         if (file_exists($this->DATABASE_LOCATION . $database_name)) {
             if (is_dir($database_name))
                 $dir_handle = opendir($database_name);
@@ -60,47 +81,128 @@ class Database
     }
 
     /**
-     * creates a file in the current directory, inside the latest_used_database
+     * @return string
+     */
+    public function getLatestDatabaseUsed()
+    {
+        return $this->latest_database_used;
+    }
+
+    /**
+     * @param string $latest_database_used
+     */
+    public function setLatestDatabaseUsed($latest_database_used)
+    {
+        $this->latest_database_used = $latest_database_used;
+    }
+
+    /**
+     * creates a file in the current directory, inside the latest_used_database,
+     * if the database is not created, it creates it first
      * @param $table_name
      */
     function createTable($table_name)
     {
+        if ((!file_exists("./$this->latest_database_used/"))) {
+            mkdir($this->DATABASE_LOCATION . $this->latest_database_used, 0777);
+        }
         if (file_exists("./$this->latest_database_used/$table_name.txt")) {
             print("Table '$table_name' already exists") . PHP_EOL;
         } else {
-            mkdir($this->DATABASE_LOCATION . $this->latest_database_used, 0777);
             $file = fopen("./$this->latest_database_used/$table_name.txt", "w");
             print("Table '$table_name' is created") . PHP_EOL;
             fclose($file);
         }
 
+        $this->setLatestTableUsed($table_name);
     }
 
     /**
      * deletes table if exists
      * @param $table_name
      */
-    function deleteTable($table_name)
+    function deleteTable($table_name = "")
     {
+        if ($table_name == "") {
+            $table_name = $this->getLatestTableUsed();
+        }
         if (file_exists("./$this->latest_database_used/$table_name.txt")) {
             unlink("./$this->latest_database_used/$table_name.txt");
-            print("Table '$table_name' is deleted");
+            print("Table '$table_name' is deleted") . PHP_EOL;
         } else {
-            print("Table $table_name doesn't exist");
+            print("Table $table_name doesn't exist") . PHP_EOL;
         }
     }
 
-//    TODO add parameters
-    function insertIntoTable()
+    /**
+     * @return string
+     */
+    public function getLatestTableUsed()
     {
-        
+        return $this->latest_table_used;
     }
 
+    /**
+     * @param string $latest_table_used
+     */
+    public function setLatestTableUsed($latest_table_used)
+    {
+        $this->latest_table_used = $latest_table_used;
+    }
 
+    /**
+     * Takes a JSON representation of the data and insert it into the last used
+     * table
+     * @param $json_format
+     */
+    function insertIntoTable($json_format)
+    {
+        $table_name = $this->getLatestTableUsed();
+        $database_name = $this->getLatestDatabaseUsed();
+        $file_location = "./" . $database_name . "/" . $table_name . ".txt";
+        file_put_contents($file_location, $json_format . PHP_EOL, FILE_APPEND);
+        print("Successfully Added into table '$table_name'") . PHP_EOL;
+    }
+
+    /**
+     * gets as a parameter a keyword to search for, returns the lines (json line)
+     * that keyword is found in
+     * @param $searchfor
+     * @return string
+     */
+    function selectFromTable($searchfor)
+    {
+        $table_name = $this->getLatestTableUsed();
+        $database_name = $this->getLatestDatabaseUsed();
+
+
+        $file_location = "./" . $database_name . "/" . $table_name . ".txt";
+
+        if (!file_exists($file_location)) {
+            print("Table '$table_name' does not exist to select from it").PHP_EOL;
+        } else {
+            // get the file contents, assuming the file to be readable (and exist)
+            $contents = file_get_contents($file_location);
+            // escape special characters in the query
+            $pattern = preg_quote($searchfor, '/');
+            // finalise the regular expression, matching the whole line
+            $pattern = "/^.*$pattern.*\$/m";
+//         search, and store all matching occurences in $matches
+//        TODO The problem here is that it is searching for anything that contains this expression, I don't want this I want it to search for absolutely the same whole expression
+            if (preg_match_all($pattern, $contents, $matches)) {
+                $result_found = "Result found: \n" . implode("\n", $matches[0]) . PHP_EOL;
+                return $result_found;
+            } else {
+                return "No matches found".PHP_EOL;
+            }
+        }
+    }
 }
 
 $database_instance = new Database();
-//$database_instance->createDatabase("NewDatabase");
+$database_instance->createDatabase("NewDatabase");
 $database_instance->createTable("student");
-//$database_instance->deleteTable("student");
-//$database_instance->deleteDatabase("NewDatabase");
+$database_instance->insertIntoTable("whatever");
+$database_instance->deleteTable("student");
+$database_instance->deleteDatabase("NewDatabase");
+print($database_instance->selectFromTable("whatever"));
