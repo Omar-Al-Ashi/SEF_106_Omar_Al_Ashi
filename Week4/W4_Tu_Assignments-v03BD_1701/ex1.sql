503524c8876e74ed87df6d0a4941f365071af5be
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-23 17:52:32 +0300

    Modify ex1.sql

--- Week4/W4_Tu_Assignments-v03BD_1701/ex1.sql
+++ Week4/W4_Tu_Assignments-v03BD_1701/ex1.sql
@@ -1,15 +1,12 @@
-
--- SELECT * from FinanceDB.FiscalYearTable;
-
 DROP DATABASE if exists FinanceDB;
 
 
 CREATE DATABASE FinanceDB;
 USE FinanceDB;
 CREATE TABLE FiscalYearTable (
-  fiscal_year Year(4) NOT NULL UNIQUE ,
-  start_date DATE NOT NULL UNIQUE ,
-  end_date DATE NOT NULL UNIQUE ,
+  fiscal_year Year(4) NOT NULL UNIQUE,
+  start_date  DATE    NOT NULL UNIQUE,
+  end_date    DATE    NOT NULL UNIQUE,
   PRIMARY KEY (fiscal_year)
 );
 
@@ -22,59 +19,62 @@ CREATE TRIGGER FiscalYearTableBeforeInsert
   BEGIN
 
     -- Check if the starting date is after the ending date
-    IF DATEDIFF(NEW.end_date,NEW.start_date)<0
+    IF DATEDIFF(NEW.end_date, NEW.start_date) < 0
     THEN
       SIGNAL SQLSTATE '45000'
-      SET MESSAGE_TEXT = 'End date must be after starting date';
+      SET MESSAGE_TEXT = 'end_date must be after start_date';
     END IF;
 
-
-
-    IF DATEDIFF(NEW.end_date,NEW.start_date)<364
-       OR DATEDIFF(NEW.end_date,NEW.start_date)>365
+    -- Check if the end_date is ahead of the start_date by more than a year
+    IF DATEDIFF(NEW.end_date, NEW.start_date) < 364
+       OR DATEDIFF(NEW.end_date, NEW.start_date) > 365
     THEN
       SIGNAL SQLSTATE '45000'
       SET MESSAGE_TEXT = 'NOT a full fiscal year';
     END IF;
 
+    -- Check if the start_date is between 1 and 31
+    IF DAY(NEW.start_date) < 1
+       OR DAY(NEW.start_date) > 31
+    THEN
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Day should be between 1 and 31';
+    END IF;
 
-    # s2 s1 E2
+    -- Check if the end_date is between 1 and 31
+    IF DAY(NEW.end_date) < 1
+       OR DAY(NEW.end_date) > 31
+    THEN
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Day should be between 1 and 31';
+    END IF;
+
+    -- check for overlapping entries
     IF EXISTS
     (
         SELECT *
-        FROM FiscalYearTable
-        WHERE NEW.start_date <= start_date
-              AND NEW.end_date >= start_date
-
+        FROM FinanceDB.FiscalYearTable
+        WHERE (NEW.start_date >= start_date
+               AND NEW.end_date >= start_date)
+              OR (NEW.start_date <= start_date
+                  AND NEW.end_date <= end_date)
     )
     THEN
       SIGNAL SQLSTATE '45000'
       SET MESSAGE_TEXT = 'overlapping date entries are not supported.';
     END IF;
-    # s2 e1 e2
-    IF EXISTS
-    (
-        SELECT *
-        FROM FiscalYearTable
-        WHERE NEW.start_date <= end_date
-              AND NEW.end_date >= end_date
-
-    )
-    THEN
-      SIGNAL SQLSTATE '45000'
-      SET MESSAGE_TEXT = 'overlapping date entries are not supported.';
-    END IF;
-
 
   END !
 DELIMITER ;
 
--- Test when end date is before start date:
+-- A working insert example
 INSERT INTO FinanceDB.FiscalYearTable
-VALUES ('1997','1997-08-17','1998-08-17');
+VALUES ('1999', '1999-09-1', '2000-08-30');
 
-
--- To check for if start_date is before end_date
+# Test when end date is before start date:
 INSERT INTO FinanceDB.FiscalYearTable
-VALUES ('1999','1999-08-17','1998-08-17');
+VALUES ('1997', '1997-08-17', '1998-08-17');
 
+# To check for if start_date is before end_date
+INSERT INTO FinanceDB.FiscalYearTable
+VALUES ('1999', '1999-08-17', '1998-08-17');
