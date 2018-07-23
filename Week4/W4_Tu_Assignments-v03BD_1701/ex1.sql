5863cb0bbfd5025b72a492cf3fddcc3d7d619f37
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-23 15:42:18 +0300

    Modify ex1 in Week4

--- Week4/W4_Tu_Assignments-v03BD_1701/ex1.sql
+++ Week4/W4_Tu_Assignments-v03BD_1701/ex1.sql
@@ -1,6 +1,24 @@
-CREATE database FinanceDB;
-CREATE table FiscalYearTable (
-  start_time DATE NOT NULL ,
-  end_time DATE NOT NULL ,
-  fiscal_year VARCHAR(10)
+
+CREATE database if not exists FinanceDB;
+CREATE table FinanceDB.FiscalYearTable (
+  start_date DATE NOT NULL UNIQUE ,
+  end_date DATE NOT NULL UNIQUE ,
+  fiscal_year VARCHAR(10) UNIQUE
 );
+
+
+
+delimiter $$
+create trigger myTrigger
+  before insert on FinanceDB.FiscalYearTable
+  for each row
+  begin
+    if NEW.start_date = 2013-12-12 then
+      set NEW.start_date= 2011-12-12;
+    end if;
+  end$$
+
+
+
+INSERT INTO FinanceDB.FiscalYearTable (start_date, end_date, fiscal_year)
+VALUES ("2013-12-12", "2011-9-30", "2010");
