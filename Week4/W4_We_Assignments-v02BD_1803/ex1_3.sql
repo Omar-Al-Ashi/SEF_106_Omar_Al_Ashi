1242385d34ce47e0903f37441469eb44ac15778d
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-25 15:15:14 +0300

    Edit ex1_3

--- Week4/W4_We_Assignments-v02BD_1803/ex1_3.sql
+++ Week4/W4_We_Assignments-v02BD_1803/ex1_3.sql
@@ -1,10 +1,15 @@
 -- What are the top 3 countries from which customers are originating?
 
 SELECT
-  customer_list.country AS 'Country',
-  count(*)              as count
-from customer_list
-  inner join customer on customer_list.ID = customer.customer_id
-group by customer_list.country
-order by count DESC
-limit 3;
+  country.country AS 'Country',
+  COUNT(*)        AS 'Count'
+FROM country
+  INNER JOIN city
+    ON country.country_id = city.country_id
+  Inner JOIN address
+    ON city.city_id = address.city_id
+  INNER JOIN customer
+    ON address.address_id = customer_id
+group by country.country_id
+ORDER BY Count DESC
+LIMIT 3;
