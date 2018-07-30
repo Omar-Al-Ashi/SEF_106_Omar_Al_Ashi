9b765ce4276ae6cf1c1e091d6bfbe2a2244dd668
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-07-30 22:49:29 +0300

    Add login page

---
+++ Week5/W5_Mo_Assignments-v02BD_1702/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<div class="login" id="login"
+     style="border-style: solid;border-width: 3px;width: 400px">
+    <h1>Login</h1>
+    <form action="/new_assignment/OrderProcess.php" method="post">
+
+        <strong><label>Email Address:</label></strong><br><input type="email"
+                                                                 name="login_email"
+                                                                 placeholder="email address"><br>
+        <strong><label>first name</label></strong><br><input type="text"
+                                                           name="first_name"
+                                                           placeholder="first name"><br>
+        <strong><label>last name</label></strong><br><input type="text"
+                                                             name="last_name"
+                                                             placeholder="last name"><br>
+        <input type="submit" value="login">
+
+    </form>
+</div>
+</body>
+</html>
+<?php
+
---
+++ Week5/W5_Mo_Assignments-v02BD_1702/register.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sef-omar
+ * Date: 30/07/18
+ * Time: 09:11 م
+ */
