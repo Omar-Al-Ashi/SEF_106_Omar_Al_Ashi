dd18fb717651ee11cf651e83bcb624fb500924f4
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-08-07 15:20:58 +0300

    Add transition animation to disks

--- Week6/W6_Mo_Assignments-v02BD_1703/javascript/HanoiSolver.js
+++ Week6/W6_Mo_Assignments-v02BD_1703/javascript/HanoiSolver.js
@@ -15,9 +15,9 @@ function stepsToSolveHanoi(num_of_disks, source_rod, destination_rod, auxiliary_
         stepsToSolveHanoi(num_of_disks - 1, source_rod, auxiliary_rod, destination_rod);
 
         // Move the remaining disk to the destination peg.
-        console.log('Move disk from Tower ', source_rod, ' to Tower ', destination_rod);
+        console.log('Move disk from ', source_rod, ' to ', destination_rod);
 
-        var text = 'Move disk from Tower ' + source_rod + ' to Tower ' + destination_rod
+        var text = 'Move disk from ' + source_rod + ' to ' + destination_rod
         addText(text);
         // document.getElementById("process").innerHTML = ('Move disk from Tower ', source_rod, ' to Tower ' , destination_rod);
 
@@ -27,7 +27,7 @@ function stepsToSolveHanoi(num_of_disks, source_rod, destination_rod, auxiliary_
     document.getElementById("text").innerHTML = "started playing";
     document.getElementById("text").style.color = "green";
     document.getElementById("text").style.textAlign = "center";
-    move("disk1", 540, 560);
+    move("disk1", 600, 560);
     // move("disk2", 5, 160);
     // move("disk3", 1050, 260)
 
@@ -50,25 +50,56 @@ function move(disk_number, x_position, y_position) {
     var disk = document.getElementById(disk_number);
 
     if (disk) {
-        var x_pos = 0;
-        var y_pos = 0;
-        var id = setInterval(frame, 5);
-        function frame() {
-            if (x_pos== x_position & y_pos == y_position) {
-                clearInterval(id);
-            } else {
-                pos++;
-                disk.style.top = x_position + 'px';
-                disk.style.left = y_position + 'px';
-            }
-        }
-
         // var pos = 0;
-        //
-        // disk.style.position = "absolute";
-        // disk.style.left = x_position + 'px';
-        // disk.style.top = y_position + 'px';
+        // var id = setInterval(frame, 5);
+        // function frame() {
+        //     if (pos == position) {
+        //         clearInterval(id);
+        //     } else {
+        //         pos++;
+        //         disk.style.top = pos + 'px';
+        //         disk.style.left = pos + 'px';
+        //     }
+        // }
+
+
+        disk.style.position = "absolute";
+        disk.style.left = x_position + 'px';
+        disk.style.top = y_position + 'px';
+
     }
 }
+//
+// function animateTo(settings) {
+//     var elem = settings.element;
+//     var ease = settings.ease;
+//
+//     var start = { left: elem.offsetLeft, top: elem.offsetTop };
+//
+//     var lastTime = new Date().getTime();
+//     var timeLeft = settings.totalTime;
+//
+//     function update() {
+//         var currentTime = new Date().getTime();
+//         var elapsed = currentTime - lastTime;
+//         timeLeft -= elapsed;
+//         lastTime = currentTime;
+//
+//         var percentDone = 1 - timeLeft/settings.totalTime;
+//
+//         elem.style.top = ease(start.top, settings.top, percentDone) + "px" ;
+//         elem.style.left = ease(start.left, settings.left, percentDone) + "px" ;
+//
+//         if(timeLeft > 0) {
+//             setTimeout(update, 33);
+//         }
+//     }
+//
+//     update();
+// }
+//
+// function linearEase(start, end, percent) {
+//     return start + ((end - start) * percent);
+// }
 
 // stepsToSolveHanoiT(3, "A", "C", "B");
