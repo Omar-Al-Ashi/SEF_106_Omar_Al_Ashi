c36df1241c037deb8ae6892c93e0cfdd7f8b1d58
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-08-10 15:55:59 +0300

    Add initial functionalities to TODO list

--- Week6/W6_Th_Assignments-v02BD_1702/ex2/js/main.js
+++ Week6/W6_Th_Assignments-v02BD_1702/ex2/js/main.js
@@ -1,1 +1,48 @@
 
+
+document.getElementById("add_button").addEventListener("click", function(){
+    removePreviousItems();
+
+    let add_text = getReferenceValue("add_title");
+    let add_description = getReferenceValue("add_description");
+
+    addToLocalStorage(add_text, add_description);
+
+    getItems();
+});
+
+
+function getReferenceValue(element_id){
+    return document.getElementById(element_id).value;
+}
+
+function addToLocalStorage(key, value) {
+    localStorage.setItem(key, value)
+}
+
+function removePreviousItems() {
+    let paras = document.getElementsByClassName('list');
+
+    while(paras[0]) {
+        paras[0].parentNode.removeChild(paras[0]);
+    }
+}
+
+function getItems() {
+    let length = localStorage.length;
+    for (let i = 0; i < length; i++) {
+        addText(localStorage.key(i));
+    }
+}
+
+function addText(text) {
+
+    var paragraph = document.createElement("p");
+    var t = document.createTextNode(text);
+    paragraph.appendChild(t);
+    paragraph.classList.add("list");
+    document.body.appendChild(paragraph);
+}
+
+getItems();
+
