7b603eb9146718907b0a924e5da8291cc7382429
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-08-15 14:59:16 +0300

    add composer to php

--- Week7/W7_We_Assignments-v02BD_1707/php/main.php
+++ Week7/W7_We_Assignments-v02BD_1707/php/main.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sef-omar
- * Date: 14/08/18
- * Time: 05:11 م
- */
+
+    // Check if we have parameters w1 and w2 being passed to the script through the URL
+    if (isset($_GET["t1"])) {
+
+        // Put the two words together with a space in the middle to form "hello world"
+        $hello = $_GET["t1"];
+
+        // Print out some JavaScript with $hello stuck in there which will put "hello world" into the javascript.
+        echo $hello;
+    }
