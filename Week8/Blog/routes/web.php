35104b6b0a4f66dea9d60ae9225896489b9a150e
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-08-18 18:32:23 +0300

    Add navigation and routes

--- Week8/Blog/routes/web.php
+++ Week8/Blog/routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Route::get('/blog_posts', function () {
     return view("blog_posts");
 });
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
