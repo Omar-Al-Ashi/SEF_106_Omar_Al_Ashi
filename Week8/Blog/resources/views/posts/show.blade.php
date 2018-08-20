51776e7a00507e52e659cdc81f4cb0732f5d30df
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-08-20 11:29:50 +0300

    Add user authentication using artisan

--- Week8/Blog/resources/views/posts/show.blade.php
+++ Week8/Blog/resources/views/posts/show.blade.php
@@ -11,4 +11,10 @@
     <a href="{{url("/posts/$post->id/edit")}}" class="btn btn-default">
         <button class="btn btn-default">Edit</button>
     </a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right' ])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!! Form::close() !!}
+
 @endsection
