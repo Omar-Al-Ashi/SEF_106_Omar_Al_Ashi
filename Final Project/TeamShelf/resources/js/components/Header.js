7351d735b3f7c5bc118f89b6d1ccdaf0f2b1dfcc
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-09-14 16:06:34 +0300

    Add Header, Footer

---
+++ Final Project/TeamShelf/resources/js/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <div className="container" style={{backgroundColor: "blue", textAlign: "center"}}>
+                This is the header
+            </div>
+        );
+    }
+}
