d5068d6f390135ce3a805d49964e0a4ee75a5ab9
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-09-19 16:39:44 +0300

    Add viewAll component to render all graduates cards

---
+++ Final Project/TeamShelf/resources/js/Pages/profile/ViewAll.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ProfileBox from "../../components/ProfileBox";
+
+export default class ViewAll extends Component {
+    render(){
+        return(
+            <div>
+                <ProfileBox/>
+            </div>
+        );
+    }
+}
