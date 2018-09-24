7788f8d067109a575e6bd2de5db6ac7dcef5b72a
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-09-24 17:22:10 +0300

    Add GraduateInfo component to view all graduate info

---
+++ Final Project/TeamShelf/resources/js/Pages/profile/GraduateInfo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class GraduateInfo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Graduate Info Fuckerss</h1>
+                {console.log("Graduate info fuckers")}
+            </div>
+        );
+    }
+}
