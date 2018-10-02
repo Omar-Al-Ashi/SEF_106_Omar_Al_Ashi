48b91eeba075ed4ded18e34b37a8c4a0d925b19d
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-02 17:51:50 +0300

    Add GraduateInfo component to view graduate's info

--- Final Project/TeamShelf/resources/js/Pages/Profile/GraduateInfo.js
+++ Final Project/TeamShelf/resources/js/Pages/Profile/GraduateInfo.js
@@ -3,16 +3,17 @@ import axios from "axios";
 import 'font-awesome/css/font-awesome.min.css';
 
 export default class GraduateInfo extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
             admins: [],
+            id: '',
             dob: '',
             email: '',
             gender: '',
             phone: '',
-            selectedFile: null,
+            selectedFile: '',
             first_name: '',
             last_name: '',
             residency_location: '',
@@ -39,18 +40,52 @@ export default class GraduateInfo extends Component {
             github: '',
         };
 
+        // // TODO uncomment the url when I fix the routing problem
+        // axios.get('/api' + this.props.location.pathname).then(response => {
+        //     this.setState({
+        //         dob: response.data[0].DOB,
+        //         id: response.data[0].id,
+        //         email: response.data[0].email,
+        //         certificate_name: response.data[0].certificate_degree_name,
+        //         company_name: response.data[0].company_name,
+        //         phone: response.data[0].contact_number,
+        //         education_description: response.data[0].description,
+        //         residency_location: response.data[0].residency_location,
+        //         first_name: response.data[0].first_name,
+        //         gender: response.data[0].gender,
+        //         github: response.data[0].github,
+        //         experience_starting_date: response.data[0].starting_date,
+        //         education_starting_date: response.data[0].starting_date,
+        //         experience_ending_date: response.data[0].ending_date,
+        //         education_ending_date: response.data[0].ending_date,
+        //         gpa: response.data[0].grade_gpa,
+        //         institute: response.data[0].institute_university_name,
+        //         company_location: response.data[0].job_location,
+        //         title: response.data[0].job_title,
+        //         skills: response.data[0].skill_set_name,
+        //         job_location: response.data[0].job_location,
+        //         last_name: response.data[0].last_name,
+        //         linkedin: response.data[0].linkedin,
+        //         major: response.data[0].major,
+        //         skill_level: response.data[0].skill_level,
+        //         skill_set_name: response.data[0].skill_set_name,
+        //         selectedFile: response.data[0].user_image,
+        //     });
+        //     console.log(response);
+        //     console.log("The image is:" + this.state.selectedFile);
+        // }).catch(errors => {
+        //     console.log(errors.response);
+        // })
+
     }
 
 
     componentWillMount() {
-
-        console.log("the route is " + this.props.location.pathname);
-
         // TODO uncomment the url when I fix the routing problem
         axios.get('/api' + this.props.location.pathname).then(response => {
-            // axios.get('/api/graduate/1').then(response => {
             this.setState({
                 dob: response.data[0].DOB,
+                id: response.data[0].id,
                 email: response.data[0].email,
                 certificate_name: response.data[0].certificate_degree_name,
                 company_name: response.data[0].company_name,
@@ -77,9 +112,8 @@ export default class GraduateInfo extends Component {
                 skill_set_name: response.data[0].skill_set_name,
                 selectedFile: response.data[0].user_image,
             });
-            console.log(response)
-        }).then(response => {
             console.log(response);
+            console.log("The image is:" + this.state.selectedFile);
         }).catch(errors => {
             console.log(errors.response);
         })
@@ -91,12 +125,16 @@ export default class GraduateInfo extends Component {
             <div className='background'>
                 <div className="mainDetails">
                     <div id="headshot" className="quickFade">
-                        <img src={(this.state.selectedFile)}
-                             alt={this.state.first_name + this.state.last_name}/>
+                        {console.log('the file state is: ' + this.state.selectedFile)}
+
+                        <img
+                            // src={require('../../../storage/app/public/images/'+this.props.user_image)}
+                            src={require('../../../../storage/app/public/images/1.png')}
+                            alt={this.state.first_name + this.state.last_name}/>
                     </div>
 
                     <div id="name">
-                        <h1 className="quickFade delayTwo">{this.state.first_name + this.state.last_name}</h1>
+                        <h1 className="quickFade delayTwo">{this.state.first_name + " " + this.state.last_name}</h1>
                         <h2 className="quickFade delayThree">{this.state.title}</h2>
                     </div>
 
@@ -104,7 +142,7 @@ export default class GraduateInfo extends Component {
                         <ul>
                             <li>e: <a
                                 href={"mailto:" + this.state.email + "?subject=We Are" +
-                                " interested in your Resume&body=Dear Mr. " + this.props.last_name
+                                " interested in your Resume&body=Dear Mr. " + this.state.last_name
                                 + ".\n We are interested in your CV"}
                                 target="_blank">{this.state.email}</a></li>
                             <li>m: {this.state.phone}</li>
@@ -121,10 +159,11 @@ export default class GraduateInfo extends Component {
                             </div>
 
                             <div className="sectionContent">
-                                <p>Gender: {this.state.gender}</p>
-                                <p>Date of birth: {this.state.dob}</p>
-                                <p>Residency
-                                    Location: {this.state.residency_location}</p>
+                                <p><strong>Gender: </strong> {this.state.gender}
+                                </p>
+                                <p><strong>Date of birth: </strong> {this.state.dob}</p>
+                                <p><strong>Residency
+                                    Location: </strong>{this.state.residency_location}</p>
                             </div>
                         </article>
                         <div className="clear"></div>
