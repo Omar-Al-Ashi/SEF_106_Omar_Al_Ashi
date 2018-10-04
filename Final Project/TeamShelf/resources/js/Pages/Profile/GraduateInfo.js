e58d9f103508cfebc4ada1e80aac136258a2138a
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-04 10:28:31 +0300

    Update GraduateInfo to remove unneccessary requests

--- Final Project/TeamShelf/resources/js/Pages/Profile/GraduateInfo.js
+++ Final Project/TeamShelf/resources/js/Pages/Profile/GraduateInfo.js
@@ -39,44 +39,6 @@ export default class GraduateInfo extends Component {
             linkedin: '',
             github: '',
         };
-
-        // // TODO uncomment the url when I fix the routing problem
-        // axios.get('/api' + this.props.location.pathname).then(response => {
-        //     this.setState({
-        //         dob: response.data[0].DOB,
-        //         id: response.data[0].id,
-        //         email: response.data[0].email,
-        //         certificate_name: response.data[0].certificate_degree_name,
-        //         company_name: response.data[0].company_name,
-        //         phone: response.data[0].contact_number,
-        //         education_description: response.data[0].description,
-        //         residency_location: response.data[0].residency_location,
-        //         first_name: response.data[0].first_name,
-        //         gender: response.data[0].gender,
-        //         github: response.data[0].github,
-        //         experience_starting_date: response.data[0].starting_date,
-        //         education_starting_date: response.data[0].starting_date,
-        //         experience_ending_date: response.data[0].ending_date,
-        //         education_ending_date: response.data[0].ending_date,
-        //         gpa: response.data[0].grade_gpa,
-        //         institute: response.data[0].institute_university_name,
-        //         company_location: response.data[0].job_location,
-        //         title: response.data[0].job_title,
-        //         skills: response.data[0].skill_set_name,
-        //         job_location: response.data[0].job_location,
-        //         last_name: response.data[0].last_name,
-        //         linkedin: response.data[0].linkedin,
-        //         major: response.data[0].major,
-        //         skill_level: response.data[0].skill_level,
-        //         skill_set_name: response.data[0].skill_set_name,
-        //         selectedFile: response.data[0].user_image,
-        //     });
-        //     console.log(response);
-        //     console.log("The image is:" + this.state.selectedFile);
-        // }).catch(errors => {
-        //     console.log(errors.response);
-        // })
-
     }
 
 
@@ -119,18 +81,21 @@ export default class GraduateInfo extends Component {
         })
     }
 
-    render(props) {
+    render() {
 
         return (
             <div className='background container resumeContainer'>
                 <div className="mainDetails">
                     <div id="headshot" className="quickFade">
                         {console.log('the file state is: ' + this.state.selectedFile)}
-
-                        <img
-                            // src={require('../../../storage/app/public/images/'+this.props.user_image)}
-                            src={require('../../../../storage/app/public/images/1.png')}
-                            alt={this.state.first_name + this.state.last_name}/>
+                        {this.state.selectedFile.trim() === "" ? (
+                            'nothing'
+                        ) : (
+                            <img
+                                // src={require('../../../storage/app/public/images/'+this.props.user_image)}
+                                src={require('../../../../storage/app/public/images/'+ this.state.selectedFile)}
+                                alt={this.state.first_name + this.state.last_name}/>
+                        )}
                     </div>
 
                     <div id="name">
@@ -157,6 +122,7 @@ export default class GraduateInfo extends Component {
                             <div className="sectionTitle">
                                 <h1>Personal Profile</h1>
                             </div>
+
 
                             <div className="sectionContent">
                                 <p><strong>Gender: </strong> {this.state.gender}
@@ -205,13 +171,11 @@ export default class GraduateInfo extends Component {
                         </section>
                     </section>
                 </div>
-                <div>
+                <div className='socialMediaContainer'>
                     <a
                         href={'http://' + this.state.linkedin}><i
-                        className="fa fa-linkedin fa-2x"/>
+                        className="fa fa-linkedin fa-2x marginRight"/>
                     </a>
-                </div>
-                <div>
                     <a
                         href={'http://' + this.state.github}><i
                         className="fa fa-github fa-2x"/>
