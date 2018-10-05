027693162ea344ce2b50aa118f5334260ef3016c
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-05 13:48:14 +0300

    Modified api routes

--- Final Project/TeamShelf/routes/api.php
+++ Final Project/TeamShelf/routes/api.php
@@ -22,7 +22,7 @@ Route::post('register', 'API\PassportController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('get-details', 'API\PassportController@getDetails');
-//    Route::get('allGraduatesWithInfo', 'API\GraduateProfileController@returnAllGraduatesWithAllInfo');
+    Route::get('allGraduatesWithInfo', 'API\GraduateProfileController@returnAllGraduatesWithAllInfo');
 
 });
 
@@ -34,7 +34,7 @@ Route::resource('educations', 'API\EducationDetailController');
 Route::resource('graduates', 'API\GraduateProfileController');
 Route::resource('skills', 'API\SkillSetController');
 Route::resource('social_medias', 'API\SocialMediaController');
-Route::get('allGraduatesWithInfo', 'API\GraduateProfileController@returnAllGraduatesWithAllInfo');
+//Route::get('allGraduatesWithInfo', 'API\GraduateProfileController@returnAllGraduatesWithAllInfo');
 Route::get('graduate/{id}', 'API\GraduateProfileController@returnSpecificGraduateInfo');
 Route::post('graduate/store', 'API\GraduateInfoController@store');
 Route::post('graduate/edit', 'API\GraduateInfoController@edit');
