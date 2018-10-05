39176d837ac267e018fb2a20ef27b60353803dec
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-05 12:11:35 +0300

    Modified UsersTableSeeder

--- Final Project/TeamShelf/database/seeds/UserTableSeeder.php
+++ Final Project/TeamShelf/database/seeds/UserTableSeeder.php
@@ -17,6 +17,11 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => "admin",
             'email' => 'admin@gmail.com',
+            'type' => 'alumni',
+            'DOB' => '19/10/1995',
+            'gender' => 'Male',
+            'is_active' => '1',
+            'contact_number' => '78 916639',
             'user_image' => '1.png',
             'password' => bcrypt('password'),
         ]);
