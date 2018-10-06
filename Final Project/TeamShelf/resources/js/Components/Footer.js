4cebb1aca023c86c012872936186d7fa792d3fbb
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-06 13:22:24 +0300

    Update Footer component to get Year

--- Final Project/TeamShelf/resources/js/Components/Footer.js
+++ Final Project/TeamShelf/resources/js/Components/Footer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 require('../Styles/Styles.css');
 
 export default class Footer extends Component {
@@ -8,7 +9,7 @@ export default class Footer extends Component {
         return (
             <footer className="page-footer font-small blue black footer">
                 <div className="footer-copyright text-center py-3 whiteText">©
-                    2018 Copyright:
+                    {(new Date().getFullYear())} Copyright:
                     <a href="https://teamshelf.com"
                        className='whiteText'> TeamShelf.com</a>
                 </div>
