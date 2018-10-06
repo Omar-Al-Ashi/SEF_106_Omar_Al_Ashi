85ab9d21b9cc55bb910a83233fec43f08be16686
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-06 15:02:25 +0300

    Update GraduateProfileController to add search method

--- Final Project/TeamShelf/app/Http/Controllers/API/GraduateProfileController.php
+++ Final Project/TeamShelf/app/Http/Controllers/API/GraduateProfileController.php
@@ -68,7 +68,27 @@ class GraduateProfileController extends Controller
     public function search(Request $request)
     {
 //        return "Hello";
-        $allGraduates = DB::select("SELECT *
+//        $result = DB::select("SELECT *
+//FROM graduate_profiles,
+//     experience_details,
+//     education_details,
+//     skill_sets,
+//     social_media,
+//     users
+//WHERE graduate_profiles.user_id = experience_details.user_id
+//              AND graduate_profiles.user_id = education_details.user_id
+//              AND graduate_profiles.user_id = skill_sets.user_id
+//              AND graduate_profiles.user_id = social_media.user_id
+//              AND graduate_profiles.user_id = users.id
+//  /**           AND graduate_profiles.first_name LIKE '%$request->query%'*/
+//              AND graduate_profiles.first_name LIKE '%React%'
+///**   OR graduate_profiles.last_name LIKE '%$request->query%' */
+//   OR graduate_profiles.last_name LIKE '%React%'
+///**   OR skill_sets.skill_set_name LIKE '%$request->query%' */
+//   OR skill_sets.skill_set_name LIKE '%React%'
+//   ");
+
+        $result = DB::select("SELECT graduate_profiles.user_id, first_name, last_name, job_title, institute_university_name, github, linkedin, email, user_image
 FROM graduate_profiles,
      experience_details,
      education_details,
@@ -76,15 +96,16 @@ FROM graduate_profiles,
      social_media,
      users
 WHERE graduate_profiles.user_id = experience_details.user_id
-        AND graduate_profiles.user_id = education_details.user_id
-        AND graduate_profiles.user_id = skill_sets.user_id
-        AND graduate_profiles.user_id = social_media.user_id
-        AND graduate_profiles.user_id = users.id
-        AND graduate_profiles.first_name LIKE '%React%'
-   OR graduate_profiles.last_name LIKE '%React%'
-   OR skill_sets.skill_set_name LIKE '%React%';");
+              AND graduate_profiles.user_id = education_details.user_id
+              AND graduate_profiles.user_id = skill_sets.user_id
+              AND graduate_profiles.user_id = social_media.user_id
+              AND graduate_profiles.user_id = users.id
+              AND graduate_profiles.first_name LIKE '%Omar%'
+   OR graduate_profiles.last_name LIKE '%Omar%'
+   OR skill_sets.skill_set_name LIKE '%Omar%'
+   ");
 
-        return response()->json($allGraduates);
+        return response()->json($result);
 
     }
 
